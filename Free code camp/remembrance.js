e7b3c0f771c1808f3efa8219aa561ac8b473c150
Odallo <eugeneodali181@gmail.com>
2025-03-22 12:56:25 +0300

    more javascript

--- Free code camp/remembrance.js
+++ Free code camp/remembrance.js
@@ -21,13 +21,37 @@ const playerOne = {
 };*/
 
 //object constructors
-function Player(name, health){
-  this.name = name;
-  this.health = health;
-  console.log(this.name);
-  console.log(this.health);
+// function Player(name, health){
+//   this.name = name;
+//   this.health = health;
+//   console.log(this.name);
+//   console.log(this.health);
+// }
+
+// let player1 = new Player("Mark", 100);
+
+// return(player1.name);
+
+
+let globalAge = 23; // This is a global variable
+
+// This is a function - and hey, a curly brace indicating a block
+function printAge(age) {
+  var varAge = 34; // This is a function scoped variable
+
+  // This is yet another curly brace, and thus a block
+  if (age > 0) {
+    // This is a block-scoped variable that exists
+    // within its nearest enclosing block, the if's block
+    const constAge = age * 2;
+    console.log(constAge); // This will work
+  }
+
+  // Remove or comment out this line, as constAge is not accessible here
+  // console.log(constAge);
 }
 
-let player1 = new Player("Mark", 100);
+printAge(globalAge);
 
-return(player1.name);
+// Remove or comment out this line, as varAge is not accessible here
+// console.log(varAge);
